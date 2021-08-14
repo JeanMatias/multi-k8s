c829fad3aea8d02cc530118d303c3819c067406b
JeanMatias <jcsmatias84@gmail.com>
2021-08-14 13:00:45 +0100

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,17 @@
-echo "buidind docker images"
 docker build -t jean84matias/multi-client:latest -t jean84matias/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jean84matias/multi-server:latest -t jean84matias/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t jean84matias/multi-worker:latest -t jean84matias/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-echo "pushing latest docker images"
 docker push jean84matias/multi-client:latest
 docker push jean84matias/multi-server:latest
 docker push jean84matias/multi-worker:latest
 
-echo "pushing $SHA docker images"
 docker push jean84matias/multi-client:$SHA
 docker push jean84matias/multi-server:$SHA
 docker push jean84matias/multi-worker:$SHA
 
-echo "running kubernetes configs"
 kubectl apply -f k8s
 
-echo "updating deployments images"
 kubectl set image deployments/client-deployment client=jean84matias/multi-client:$SHA
 kubectl set image deployments/server-deployment server=jean84matias/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=jean84matias/multi-worker:$SHA
